6cdc123432aa7bf515118ce21df621fd05afe513
Vadim Zaliva <lord@crocodile.org>
2024-01-18 10:12:12 +0000

    wip

--- grammar.js
+++ grammar.js
@@ -1,5 +1,20 @@
+function separated_list(del, rule) {
+    return optional(separated_nonempty_list(del, rule))
+}
+
+function separated_nonempty_list(del, rule) {
+    return seq(rule, repeat(seq(del, rule)))
+}
+
+
 module.exports = grammar({
     name: 'core',
+
+    extras: $ => [
+        /\s/,
+        /--.*\n/,
+        /{-.*-}/,
+    ],
 
     rules: {
         source_file: $ => repeat1($.declaration),
@@ -7,7 +22,7 @@ module.exports = grammar({
         declaration: $ => choice(
             //$.def_declaration,
             //$.ifun_declaration,
-            //$.glob_declaration,
+            $.glob_declaration,
             //$.fun_declaration,
             //$.proc_declaration,
             //$.builtin_declaration,
@@ -89,16 +104,80 @@ module.exports = grammar({
 
         basic_type: $ => choice(
             $.integer_type,
-            $.floating_type)
+            $.floating_type),
 
+        glob_ctype_attribute: $ => seq(
+            "[",
+            "ail_ctype",
+            "=",
+            $.core_ctype,
+            "]"),
 
-    },
+        glob_declaration: $ => seq(
+            "glob",
+            $.sym,
+            ":",
+            $.core_type,
+            $.glob_ctype_attribute,
+            ":=",
+            $.expr),
 
-    extras: $ => [
-        /\s/,
-        /--.*\n/,
-        /{-.*-}/,
-    ],
+        expr: $ => choice(
+            seq("(", $.expr, ")"),
+            seq("pure", "(", $.pexpr,")"),
+            seq("memop", "(", $.memop_op, ",", separated_list(",", $.pexpr), ")"),
+            seq("let", $.pattern, "=", $.pexpr, "in", $.expr),
+            seq("if", $.pexpr, "then", $.expr, "else", $.expr),
+            seq("case", $.pexpr, "of", repeat($.pattern_pair($.expr)), "end"),
+            seq("pcall", "(", $.name, ")"),
+            seq("pcall", "(", $.name, ",", separated_nonempty_list(",", $.pexpr),")"),
+            seq("ccal", "(", $.pexpr, ",", $.pexpr, ")"),
+            seq("ccal", "(", $.pexpr, ",", $.pexpr, ",", separated_nonempty_list(",",$.pexpr), ")"),
+            $.paction,
+            seq("unseq", "(", separated_list(",", $.expr), ")"),
+            seq("let", "weak", $.pattern, "=", $.expr, "in", $.expr),
+            seq($.expr, ";", $.expr),
+            seq("let", "strong", $.pattern, "=", $.expr, "in", $.expr),
+            seq("bound", "(", $.expr, ")"),
+            seq("save", $.sym, ":", $.core_base_type),
+            seq("run", $.sym, "(", separated_list(",", $.pexpr), ")"),
+            seq("nd", "(", separated_list(",", $.expr), ")"),
+            seq("par", "(", separated_list(",", $.expr), ")")),
 
+        core_type: $ => choice(
+            $.core_base_type,
+            seq("eff", $.core_base_type)),
+
+        pexpr: $=> choice(
+            seq("(", pexpr, ")"),
+            seq("undef", "(", $.ub, ")"),
+            seq("error", "(", $.string, ",", $.pexpr, ")"),
+            $.value,
+            $.sym,
+            $.impl,
+            seq("(", $.pexpr, ",", separated_nonempty_list(",", $,pexpr), ")"),
+            $.list_pexpr,
+            seq($.ctor, "(", separated_list(",", $.pexpr), ")"),
+            seq("case", $.pexpr, "of", repeat($.pattern_pair($.pexpr)), "end"),
+            seq("array_shift", "(", $.pexpr, ",", $.core_ctype, ",", $.pexpr, ")"),
+            seq("member_shift", "(", $.pexpr, ",", $.sym, ",", ".", $.cabs_id, ")"),
+            seq("not", "(", $.pexpr, ")"),
+            seq("-", $.pexpr),
+            seq("cfunction", "(", $.pexpr, ")"),
+            seq($.pexpr, $.binary_operator, $.pexpr),
+            seq("memop", "(", $.pure_memop_op, ",", separated_list(",", $.pexpr), ")"),
+            seq("(", "struct", $.sym, ")", "{", separated_list (",", $.member), "}"),
+            seq("(", "union", $.sym, ")", "{", $.member, "}"),
+            seq($.name, "(", separated_list(",", $.pexpr), ")"),
+            seq("let", $.pattern, "=", $.pexpr, "in", $.pexpr),
+            seq("if", $.pexpr, "then", $.pexpr, "else", $.pexpr),
+            seq("is_scalar", "(", $.pexpr, ")"),
+            seq("is_integer", "(", $.pexpr, ")"),
+            seq("is_signed","(", $.pexpr, ")"),
+            seq("is_unsigned", "(", $.pexpr, ")"),
+            seq("are_compatible", "(", $.pexpr, ",", $.pexpr, ")")
+        )
+
+    }
 });
 
