1f7082e810206307f9a0458ae642c6a5caad32e6
Vadim Zaliva <lord@crocodile.org>
2024-01-19 09:46:53 +0000

    better test script output

--- test.sh
+++ test.sh
@@ -3,18 +3,30 @@
 ok_count=0
 error_count=0
 
+# Terminal color codes
+if [ -t 1 ]; then
+    green='\033[0;32m'
+    red='\033[0;31m'
+    no_color='\033[0m'
+else
+    green=''
+    red=''
+    no_color=''
+fi
+
 for file in tests/*.core; do
-    echo -n "Parsing $file "
+    file_name=$(basename "$file")
+    echo -n "$file_name: "
     if tree-sitter parse "$file" 2>/dev/null | grep -q 'ERROR'; then
-        echo "ERROR"
+        echo -e "${red}ERROR${no_color}"
         ((error_count++))
     else
-        echo "OK"
+        echo -e "${green}OK${no_color}"
         ((ok_count++))
     fi
 done
 
 echo ""
-echo "Tests Passed: $ok_count"
-echo "Total Failed: $error_count"
+echo -e "Tests Passed: ${green}$ok_count${no_color}"
+echo -e "Total Failed: ${red}$error_count${no_color}"
 
