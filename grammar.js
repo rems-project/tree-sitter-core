167c94b8a7edec9e756bc92dac2b6be11c187f7c
Vadim Zaliva <lord@crocodile.org>
2024-01-19 17:41:58 +0000

    tuples may have more than 2 elements

--- grammar.js
+++ grammar.js
@@ -133,7 +133,7 @@ module.exports = grammar({
             $.sym,
             $.impl,
             seq("(", separated_nonempty_list(",", $.pexpr), ")"),
-            seq('(', $.pexpr, ',', $.pexpr, ')'),
+            seq('(', $.pexpr, ',', separated_nonempty_list(',', $.pexpr), ')'),
             $.list_pexpr,
             seq($.ctor, "(", separated_list(",", $.pexpr), ")"),
             seq("case", $.pexpr, "of", repeat(seq("|", $.pattern, "=>",$.pexpr)), "end"),
