b66500b1e8b797bc7159b85b1cb5aeb85372d5b4
Vadim Zaliva <lord@crocodile.org>
2024-01-19 10:50:02 +0000

    testing ordering (by size)

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ else
     no_color=''
 fi
 
-for file in tests/*.core; do
+for file in $(find tests/ -name '*.core' -type f -print0 | xargs -0 du -b | sort -nr | cut -f2-); do
     file_name=$(basename "$file")
     echo -n "$file_name: "
     if tree-sitter parse "$file" 2>/dev/null | grep -q 'ERROR'; then
