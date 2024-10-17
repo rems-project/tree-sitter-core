7e178363f7a776ccc9fdc63f002738f2a63ea240
Vadim Zaliva <lord@crocodile.org>
2024-10-17 15:05:45 -0700

    saving .ast files

--- test.sh
+++ test.sh
@@ -16,8 +16,14 @@ fi
 
 for file in $(find tests/ -name '*.core' -type f -print0 | xargs -0 du -b | sort -nr | cut -f2-); do
     file_name=$file
+    ast_file="${file_name%.core}.ast"
     echo -n "$file_name: "
-    if tree-sitter parse "$file" 2>/dev/null | grep -q 'ERROR'; then
+
+    # Run tree-sitter parse and save the output
+    parse_output=$(tree-sitter parse "$file" 2>/dev/null)
+    echo "$parse_output" > "$ast_file"
+
+    if echo "$parse_output" | grep -q 'ERROR'; then
         echo -e "${red}ERROR${no_color}"
         ((error_count++))
     else
